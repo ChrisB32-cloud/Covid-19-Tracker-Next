714184400fa477ebbb92015ab16fd54f0d66b61e
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-07-12 15:33:09 -0500

    Working through bug with populating the fields

--- components/PopulateField.js
+++ components/PopulateField.js
@@ -1,39 +1,47 @@
 import React from 'react';
 // import './PopulateField.css';
+import styles from '../styles/PopulateField.module.css'
 
 const PopulateField = (props) => {
-    // const { state, positive, hospitalized, death } = props.feilds;
+    const { Country, Confirmed, Active, Recovered, Deaths } = props.feilds;
 
     // const mortRatePre = (death / positive) * 100;
     // const mortRate = mortRatePre.toFixed(2);
 
-    // const CheckFields = () => {
-    //     if (!state) {
-    //         return <h1>Select State for details</h1>;
-    //     } else {
-    //         // console.log('value changed to: ', state);
-    //         return (
-    //             <div className="popFieldBox">
-    //                 <h1>State: {state === null ? 'N/A' : state} </h1>
-    //                 <h2>Positive: {positive === null ? 'N/A' : positive} </h2>
-    //                 <h2>Hospitalized :{hospitalized === null ? 'N/A' : hospitalized}</h2>
-    //                 <h2>Deaths: {death === null ? 'N/A' : death} </h2>
-    //                 <h2>Mortality: {mortRate === 'NaN' ? 'N/A' : mortRate}% </h2>
-    //             </div>
-    //         );
-    //     }
-    // };
+    const CheckFields = () => {
+        if (props.feilds === []) {
+            return <h1>Select Country for details</h1>;
+        } else {
+            // console.log('value changed to: ', state);
+            return (
+                <div className={styles.popContainer}>
+                    <h1>State: {Country === null ? 'N/A' : Country} </h1>
+                    <h2>Positive: {Confirmed === null ? 'N/A' : Confirmed} </h2>
+                    <h2>Hospitalized :{Active === null ? 'N/A' : Active}</h2>
+                    <h2>Deaths: {Recovered === null ? 'N/A' : Recovered} </h2>
+                    <h2>Mortality: {Deaths === 'NaN' ? 'N/A' : Deaths} </h2>
+                </div>
+            );
+        }
+    };
 
-    // const ShowStateData = () => {
-    //     return <CheckFields />;
-    // };
-    // return <ShowStateData />;
-    console.log(props.feilds);
-    return (
-        <div>
-            <h1>Cases in Country</h1>
-        </div>
-    )
+    const ShowStateData = () => {
+        return <CheckFields />;
+    };
+    return <ShowStateData />;
+
 };
 
 export default PopulateField;
+
+
+ // console.log(props.feilds);
+    // return (
+    //     <div className={styles.popContainer}>
+    //         <h1>Cases in Country {Country} </h1>
+    //         <h2>Confirmed: {Confirmed} </h2>
+    //         <h2>Active: {Active} </h2>
+    //         <h2>Recovered: {Recovered} </h2>
+    //         <h2>Deaths: {Deaths} </h2>
+    //     </div>
+    // )
